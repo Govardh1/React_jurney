c3b68229b2ea1102bca411fa95a68c67e7591b93
Govardh1 <govi.reddy420@gmail.com>
2024-02-21 23:22:22 +0530

    react

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},
 [React.createElement("div",{id:"child"},
 [React.createElement("h1",{},"This is the FIRST-ARRAY1 H1"),
-React.createElement("h1",{},"This is from FIRST-ARRAY2 H1")]),React.createElement("div",{id:"child"},
+React.createElement("h2",{},"This is from FIRST-ARRAY2 H2")]),React.createElement("div",{id:"child"},
 [React.createElement("h1",{},"This is the SECOND-ARRAY-1 H1"),
-React.createElement("h1",{},"This is from SECOND-ARRAY2 H1")])]);
+React.createElement("h2",{},"This is from SECOND-ARRAY2 H2")])]);
 
 
 
